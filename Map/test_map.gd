46b4e96e6cb92897c6c3e3f0919e8678166134bf
OmenF1 <owen.lingza@outlook.com>
2025-01-18 17:35:08 +0200

    basic mobs WIP

--- Map/test_map.gd
+++ Map/test_map.gd
@@ -1,21 +1,21 @@
 extends Node2D
+
 @export var mob_scene: PackedScene
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
+func _ready() -> void:
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
+# Called when the mob spawn timer times out.
 func _on_mob_spawn_timer_timeout() -> void:
-	add_mobs(5)
-	$MobSpawnTimer.start()
-
-func add_mobs(amount: int) -> void:
-	for i in amount:
-		var mob  = mob_scene.instantiate()
+	for i in range(5):
+		var mob = mob_scene.instantiate()
 		add_child(mob)
+		mob.position = Vector2(randf_range(100, 800), randf_range(100, 600)) 
+		
+	# Restart the timer after spawning the mobs (if you want continuous spawning)
+	$MobSpawnTimer.start()
